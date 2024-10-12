260c74df5f183aa14b311a7a91ba592164dcca87
Fierillo <fierillo@protonmail.com>
2024-10-12 16:40:34 -0300

    3.16.0: the "kukas" and "shitcoin" replies now have a 21% chance to appear, "kuka" reply is less agressive in a specific channel.

--- index.ts
+++ index.ts
@@ -259,14 +259,18 @@ bot.on('message', async (msg) => {
   }
 });
 
-// // Bot replies ME CHUPA LA PIJA LA OPINION DE LAS KUKAS when users write "peron*", "kuka*", "kirchner*", "zurdo*"
-// bot.onText(/(?<=\s|^)(peron|kuka|kirchner|zurdo)\w*/i, (msg) => {
-//   bot.sendMessage(msg.chat.id, 'ME CHUPA LA PIJA LA OPINION DE LAS KUKAS');
-// });
+// Bot replies ME CHUPA LA PIJA LA OPINION DE LAS KUKAS when users write "peron*", "kuka*", "kirchner*", "zurdo*"
+bot.onText(/(?<=\s|^)(peron|kuka|kirchner|zurdo)\w*/i, (msg) => {
+  if (Math.random() <= 0.21) {
+    bot.sendMessage(msg.chat.id, msg.chat.id === -1001778459295 ? 'NO ME INTERESA LA OPINION DE LAS KUKAS' : 'ME CHUPA LA PIJA LA OPINION DE LAS KUKAS');
+  }
+});
 
 // Bot replies to shitcoiners
 bot.onText(/(?<=\s|^)(eth|solana|sol |bcash|bch |polkadot|dot |cardano|ada )\w*/i, (msg) => {
-  bot.sendMessage(msg.chat.id, '🚨 ALERTA DU SHITCOINER 🚨');
+  if (Math.random() <= 0.21) {
+    bot.sendMessage(msg.chat.id, '🚨 ALERTA DU SHITCOINER 🚨');
+  }
 });
 
 bot.onText(/\/test/, (msg) => {
